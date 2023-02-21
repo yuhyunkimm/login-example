e5da3b7233dacb225cbc2031fb6542c4d95c6f91
YouHyunKim <yuhyunk7@gmail.com>
2023-02-21 11:03:12 +0900

    회원가입 로그인 구현 통합 테스트 완료

--- src/test/java/shop/mtcoding/loginexample/controller/UserControllerTest.java
+++ src/test/java/shop/mtcoding/loginexample/controller/UserControllerTest.java
@@ -1,5 +1,59 @@
 package shop.mtcoding.loginexample.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.servlet.http.HttpSession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import shop.mtcoding.loginexample.model.User;
+
+import org.springframework.http.MediaType;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 public class UserControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void join_test() throws Exception {
+        // given
+        String requestBody = "username=ssar&password=1234&email=ssar@nate.com";
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/join").content(requestBody)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
+
+        // then
+        resultActions.andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    public void login_test() throws Exception {
+        // given
+        String requestBody = "username=ssar&password=1234";
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/login").content(requestBody)
+
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
+
+        HttpSession session = resultActions.andReturn().getRequest().getSession();
+        User principal = (User) session.getAttribute("principal");
+
+        // then
+        assertThat(principal.getUsername()).isEqualTo("ssar");
+        resultActions.andExpect(status().is3xxRedirection());
+
+    }
 }
--- src/test/java/shop/mtcoding/loginexample/model/UserRepositoryTest.java
+++ src/test/java/shop/mtcoding/loginexample/model/UserRepositoryTest.java
@@ -1,8 +1,34 @@
 package shop.mtcoding.loginexample.model;
 
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @MybatisTest
 public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void findByUsernameAndPassword_test() throws Exception {
+        // given
+        ObjectMapper om = new ObjectMapper();
+
+        // when
+        // List<UserRespDto> BoardMainRespDto = boardRepository.findAllWithUser();
+        // String responseBody = om.writeValueAsString(BoardMainRespDto);
+        // System.out.println("테스트 : " + responseBody);
+
+        // // 테스트 2번
+        // // BoardMainRespDto.forEach((dto)->{
+        // // System.out.println("테스트 : "+dto.getId());
+        // // System.out.println("테스트 : "+dto.getTitle());
+        // // System.out.println("테스트 : "+dto.getUsername());
+        // // });
+
+        // then
+        // assertThat(BoardMainRespDto.get(5).getUsername()).isEqualTo("love");
+    }
 
 }
