6b2edfd770d834f8e3a85aba6ca7d6789099a13d
YouHyunKim <yuhyunk7@gmail.com>
2023-02-21 09:52:45 +0900

    환경 셋팅 완료

---
+++ src/main/java/shop/mtcoding/loginexample/LoginExampleApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.loginexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/loginexample/LoginExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.loginexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
