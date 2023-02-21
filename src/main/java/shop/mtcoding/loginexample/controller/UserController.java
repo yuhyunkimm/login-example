49b4c328f8470d498edfc4d0ecc1ac732b5cb0f8
YouHyunKim <yuhyunk7@gmail.com>
2023-02-21 10:30:08 +0900

    uset controller 생성

--- src/main/java/shop/mtcoding/loginexample/controller/UserController.java
+++ src/main/java/shop/mtcoding/loginexample/controller/UserController.java
@@ -2,9 +2,20 @@ package shop.mtcoding.loginexample.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    @PostMapping("/join")
+    public String join() {
+        return "redirect:/loginForm";
+    }
+
+    @PostMapping("/login")
+    public String login() {
+        return "redirect:/";
+    }
 
     @GetMapping("/joinForm")
     public String joinForm() {
